32cf3435aa0571af5a87d6473fc8a15fe17b7d9f
G-cake <girjeshd1234@gmail.com>
2023-10-26 16:44:54 +0530

    crna styler02 & created new component

---
+++ Styler02/App.tsx
@@ -0,0 +1,21 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView
+} from 'react-native'
+import React from 'react'
+import FlatCards from './components/FlatCards';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text>App</Text>
+        <FlatCards />
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default App
