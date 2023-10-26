ea27640a774adedf0032b6d89983df2bfa1b61b1
G-cake <girjeshd1234@gmail.com>
2023-10-26 17:30:11 +0530

    added elevated card with horizontal scroll

--- Styler02/App.tsx
+++ Styler02/App.tsx
@@ -6,13 +6,14 @@ import {
 } from 'react-native'
 import React from 'react'
 import FlatCards from './components/FlatCards';
+import ElevatedCards from './components/ElevatedCards';
 
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        {/* <Text>App</Text> */}
         <FlatCards />
+        <ElevatedCards />
       </ScrollView>
     </SafeAreaView>
   )
--- Styler02/components/FlatCards.tsx
+++ Styler02/components/FlatCards.tsx
@@ -46,7 +46,7 @@ const styles = StyleSheet.create({
         width: 100,
         height: 100,
         borderRadius:4,
-        margin: 8
+        margin: 8,
     },
     cardOne:{
         backgroundColor: '#EF5354',
